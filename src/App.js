7c6c54e346833ec60fef365e4a87f1eb1a59ad19
Maitreya61 <maitreyaoffl@gmail.com>
2023-05-20 14:50:40 +0530

    ThirdCommit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   
   
   useEffect(()=>{
-    axios.get('/markets?vs_currency=inr&order=market_cap_desc&per_page=25&page=1&sparkline=false&locale=en')
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=25&page=1&sparkline=false&locale=en')
       .then(
         res=>{
           setCoins(res.data);

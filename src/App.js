7f456cb63a2d445e7a3722de5c8b637e304e0af5
Maitreya61 <maitreyaoffl@gmail.com>
2023-05-20 14:37:58 +0530

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useEffect, useState, } from 'react';
 import './App.css';
+import axios from 'axios';
+import Coins from './Coins';
+
+// const cors = require("cors");
+
+// app.use(cors({
+//   origin: "*",
+// }))
+
 
 function App() {
+  
+  const [coins,setCoins] = useState([])
+  const[search,setSearch] = useState('')
+  
+  
+  useEffect(()=>{
+    axios.get('/markets?vs_currency=inr&order=market_cap_desc&per_page=25&page=1&sparkline=false&locale=en')
+      .then(
+        res=>{
+          setCoins(res.data);
+          
+        }
+        
+      )
+      //.catch(err => console.log(err))
+  },[])
+  
+
+  const fcoins = coins.filter((item)=>{
+    return(search.toLowerCase() ==='' ? item :item.name.toLowerCase().includes(search))
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+      <div className="nav">
+      <div className="head">
+        <h1>CRYPTO TRACKER</h1>
+        <h3>By Maitreya</h3>
+      </div>
+      <div className='inputform'>
+        <form >
+          <div>
+            <input  onChange={(e)=>setSearch(e.target.value)} placeholder='Search' type="text" />
+          </div>
+        </form>
+      </div>
+      </div>
+
+      <div>
+        {fcoins.map((coin)=>{
+          return <Coins
+          key={coin.id}
+          name={coin.name}
+          image={coin.image}
+          current={coin.current_price}
+          high={coin.high_24h}
+          low={coin.low_24h}
+          change={coin.price_change_percentage_24h}
+          />
+        })}
+      </div>
     </div>
+    
   );
 }
 
